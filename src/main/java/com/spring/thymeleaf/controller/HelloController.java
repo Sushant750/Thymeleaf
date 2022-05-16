93b6e07b1857b2720779977daa4cbddc9b1572d3
Sushant750 <sonukumaroffc@gmail.com>
2022-05-16 18:32:23 +0530

    ThymeleafBasics

---
+++ src/main/java/com/spring/thymeleaf/controller/HelloController.java
@@ -0,0 +1,53 @@
+package com.spring.thymeleaf.controller;
+
+import com.spring.thymeleaf.model.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+      return "hello";
+}
+
+@RequestMapping("/sendData")
+public ModelAndView sendData(){
+
+    ModelAndView data = new ModelAndView("data");
+   return data.addObject("message", "Increase your knowledge");
+}
+
+@RequestMapping("/student")
+public ModelAndView getStudent(){
+        ModelAndView modelAndView = new ModelAndView("student");
+    Student value = new Student();
+    value.setName("Sonu");
+    value.setScore(100);
+    return modelAndView.addObject("student", value);
+
+}
+
+
+
+    @RequestMapping("/students")
+    public ModelAndView getStudents(){
+        ModelAndView modelAndView = new ModelAndView("studentList");
+        Student student = new Student();
+        student.setName("Ravi");
+        student.setScore(99);
+
+        Student student1 = new Student();
+        student1.setName("Hari");
+        student1.setScore(98);
+
+        List<Student> studentList = Arrays.asList(student, student1);
+        return modelAndView.addObject("students", studentList);
+
+    }
+
+}
---
+++ src/main/java/com/spring/thymeleaf/model/Student.java
@@ -0,0 +1,31 @@
+package com.spring.thymeleaf.model;
+
+public class Student {
+
+    private String name;
+    private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+}
